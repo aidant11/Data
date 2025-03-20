437366fab9d8daec9230e69116add4fce38574ef
aidant11 <aidant10@nycstudents.net>
2025-03-20 15:48:16 +0000

    123

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
 
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Data.MAX);
+        Data d = new Data();
+        System.out.println(d);
+        d.repopulate();
+        System.out.println(d);
+        int[][] grid = {
+            {10, 50, 40},
+            {20, 40, 20},
+            {30,50,30}
+        };
+        d=new Data(grid);
+    }
+}
